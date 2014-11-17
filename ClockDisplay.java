2399fafd13ed845865cad5c5e476c5b1d35a7bd7
Alvaro <varofv66@gmail.com>
2014-11-17 12:32:31 +0100

    Añadida clase ClockDisplay y declarados los atributos

---
+++ ClockDisplay.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private String horaActual;
+    
+}
