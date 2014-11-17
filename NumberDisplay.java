2687af3013e6626addc207560a56553b700af2ad
Alvaro <varofv66@gmail.com>
2014-11-17 11:12:48 +0100

    Creada clase Numberdisplay y declarados atributos

---
+++ NumberDisplay.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+    public class NumberDisplay
+{
+    // instance variables - replace the example below with your own
+    private int value;
+    private int limit;
+}
